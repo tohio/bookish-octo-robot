075a45dd801462174c790c8166d7801da2c58d9d
tohio <tohio@hotmail.com>
2023-03-12 21:50:56 +0000

    added another server #minor

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+resource "aws_instance" "app_server2" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
